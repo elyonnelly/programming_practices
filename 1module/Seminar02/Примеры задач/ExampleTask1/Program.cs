e4787a21cf9ede6350bf4340412e491cdf46192c
Sergey Fomin <sdfomin@edu.hse.ru>
2019-07-01 15:41:03 +0300

    Примеры задач для семинара 2

---
+++ 1module/Seminar02/Примеры задач/ExampleTask1/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Студент: Фомин Сергей
+ * Группа: БПИ182
+ * Дата: 01.07.2019
+ * Задача: Счтитать 3 целых числа с клавиатуры, вывести среднее значение этих чисел
+ *         с точностью до трёх знаков после запятой. 
+ *         
+ */
+
+using System;
+
+namespace ExampleTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            try
+            {
+                Console.WriteLine("Введите первое число: ");
+                string input = Console.ReadLine();
+                a = int.Parse(input);
+                Console.WriteLine("Введите второе число: ");
+                b = int.Parse(Console.ReadLine()); // так короче и проще
+                Console.WriteLine("Введите третье число: ");
+                c = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException) // Ошибка при форматировании числа (присутствуют не числа, число вещественное)
+            {
+                Console.WriteLine("Ошибка при считывании числа (это не число)!");
+                return;
+            }
+            catch (OverflowException ex) // Число вылезает за пределы границ инта
+            {
+                Console.WriteLine(ex.Message); // Когда нет времени писать юзер-френдли ошибку, можно просто выводить ex.Message
+                return;
+            }
+            catch (Exception ex) // Если вдруг вылезет какой-то левый эксепшн, то ловим его общим эксепшном
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            try
+            {
+                double average = checked((a + b + c) / (double)3);
+                Console.WriteLine($"Среднее значение: {average:F3}"); // Выводим через интерполяцию строк
+                                                                      // https://docs.microsoft.com/ru-ru/dotnet/csharp/tutorials/string-interpolation а вообще такие длинные строки делать нельзя, как по мне длиннее 100 символов их стоит юзать только в крайних случаях
+            }
+            catch (OverflowException ex) // ex не используется, но лучше так не оставлять
+            {
+                Console.WriteLine("Ошибка во время вычисления среднего числа, сумма выходит за пределы инта!");
+                //Можете подумать, как избежать переполнения в формуле без приведения к лонгу
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey(); // Оставляем, чтобы программа не закрывалась при дебаге (F5) после завершения
+                               // (Если запускать без отладки (Ctrl+F5), то программа закрываться не будет
+        }
+    }
+}
---
+++ 1module/Seminar02/Примеры задач/ExampleTask2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Студент: Фомин Сергей
+ * Группа: БПИ182
+ * Дата: 01.07.2019
+ * Задача: Каждому школьнику из курса органической химии известна формула 
+ *         молекулы этилового спирта – C2H5(OH). Откуда видно, что молекула 
+ *         спирта состоит из двух атомов углерода (C), шести атомов водорода (H) 
+ *         и одного атома кислорода (O).
+ *         
+ *         По заданному количеству атомов каждого из описанных выше элементов 
+ *         требуется определить максимально возможное количество молекул спирта, 
+ *         которые могут образоваться в процессе их соединения.
+ *         
+ *         Необходимо считать нужные числа с клавиатуры и вывести ответ. 
+ *         
+ *         http://acmp.ru/index.asp?main=task&id_task=757
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long c, h, o; // лучше избегать название переменной o и l, легко спутать: o0O0oo01II1lI1 (вижак норм рисует, но не стоит)
+            try
+            {
+                Console.WriteLine("Введите количество молекул углерода: ");
+                c = long.Parse(Console.ReadLine());
+                Console.WriteLine("Введите количество молекул водорода: ");
+                h = long.Parse(Console.ReadLine());
+                Console.WriteLine("Введите количество молекул кислорода: ");
+                o = long.Parse(Console.ReadLine());
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Заданное число слишком большое!");
+                return;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода!");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            long ans = Math.Min(Math.Min(c / 2, h / 6), o / 1); // Минимум из трёх чисел
+            Console.WriteLine($"Мы можем получить {ans} молекул спирта!)");
+
+            Console.ReadKey();
+        }
+    }
+}
