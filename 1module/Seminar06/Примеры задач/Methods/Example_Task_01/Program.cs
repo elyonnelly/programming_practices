ccc6f856bfdaa74529cb3c951ac4a5e65be6abe7
sirenescx <47086248+sirenescx@users.noreply.github.com>
2019-07-01 15:50:48 +0300

    Add files via upload

---
+++ 1module/Seminar06/Примеры задач/Methods/Example_Task_01/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задача: 
+ * На вход подается целое число number. 
+ * Необходимо вывести его в обратном порядке (справа налево). 
+ * Все вычисления осуществлять в методе ReverseNumber(int number).
+ * Использовать строки, массивы, списки и циклы ЗАПРЕЩАЕТСЯ.
+ */
+
+using System;
+
+namespace Example_Task_01
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод для вычисления и вывода заданного числа в обратном порядке.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int ReverseNumber(int num)
+        {
+            if (num < 10)
+            {
+                Console.Write(num);
+                return num;
+            }
+            else
+            {
+                Console.Write(num % 10);
+                return ReverseNumber(num / 10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Введите число, которое необходимо записать в обратном порядке: ");
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Входное значение должно быть целым числом типа Integer.");
+                return;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Входное значение превосходит границы типа Integer.");
+                return;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Входное значение должно быть целым числом типа Integer.");
+                return;
+            }
+
+            Console.Write($"Число, записанное в обратном порядке: {ReverseNumber(number)}");
+            Console.ReadKey();
+        }
+    }
+}
