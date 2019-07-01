d3d6c8f19d0a968808c9d94311c11fd04aef50fa
sirenescx <47086248+sirenescx@users.noreply.github.com>
2019-07-01 16:50:07 +0300

    Add files via upload

---
+++ 1module/Seminar03/TODO задачи/Seminar_3_TODO/task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TODO_task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = 7, secondInt = 3;
+            // TODO: осуществить ввод переменных firstInt и secondInt, используя метод TryParse().
+            Console.WriteLine($"Введенные числа:\n{firstInt}\t{secondInt}");
+            // TODO: добавить проверку корректности деления.
+            Console.WriteLine(firstInt / secondInt);
+            // TODO: вывести результат деления с точностью трех знаков после запятой.
+            Console.WriteLine((double)firstInt / secondInt);
+            // TODO: заменить умножение на 4 операцией побитового сдвига.
+            Console.WriteLine(firstInt * 4);
+            // TODO: добавить повтор решения.
+        }
+    }
+}
---
+++ 1module/Seminar03/TODO задачи/Seminar_3_TODO/task_02/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task_02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_02")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e5db60fb-89bb-48bc-b2e1-12a562ba16a8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
