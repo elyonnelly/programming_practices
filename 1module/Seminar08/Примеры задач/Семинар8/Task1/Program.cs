ae56897ede3bcf40518e6d48dc6eedc7f85b7419
geremere <rad.imamow@yandex.ru>
2019-07-01 15:31:20 +0300

    sem8examples

---
+++ 1module/Seminar08/Примеры задач/Семинар8/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[rnd.Next(4, 17), rnd.Next(4, 17)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = rnd.Next(-5, 6);
+
+            PrintMatrix(matrix);
+
+            Console.WriteLine("\n====================\n");
+
+            int count = 0;
+            foreach (int item in matrix)
+                if (item < 0) count += 1;
+
+
+            if (count >= matrix.Length / 2)
+            {
+                ReplaceItems(matrix);
+                PrintMatrix(matrix);
+            }
+
+            Console.Read();
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j].ToString() + " \t");
+                Console.WriteLine();
+            }
+        }
+
+        static void ReplaceItems(int[,] matrix) // Возращаемое значение void так как массив -  
+        {// ссылочный тип данных и в метод передается не объект, а ссылка на него в куче
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0) matrix[i, j] *= -1;
+                }
+        }
+    }
+}
---
+++ 1module/Seminar08/Примеры задач/Семинар8/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.WriteLine("Введите n");
+            } while (!int.TryParse(Console.ReadLine(), out n) || n < 1);
+
+            int[] mas = new int[n];
+            for (int i = 0; i < n; i++)
+                mas[i] = rnd.Next(-100, 100);
+
+            Array.Sort(mas);
+            Array.Reverse(mas);
+
+            int count = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] < 0) count++;
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+            Array.Resize(ref mas, n - count);
+
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
