1806e8ea73577315c03282a5fdc52577f985174a
elyonnelly <alena@kotim.ru>
2019-07-03 13:39:48 +0300

    Добавлены задачи в 2.2 и примеры в 2.7

---
+++ 2module/Seminar07/Примеры решённых задач/Virtual methods/Example_Task_01/Program.cs
@@ -0,0 +1,94 @@
+﻿/* Создайте библиотеку классов Fractions, где определите три класса: 
+ * 1) Класс колоды карт Deck 
+ * 2) Унаследованный от Deck класс фракции Нильфгаард Nilfgaard 
+ * 3) Унаследованный от Deck класс фракции Скеллиге Skellige
+ * 
+ * Члены класса Deck:
+ * I. Поля:
+ * 1) Количество карт каждого типа: amountOfGoldenCards, amountOfSilverCards, amountOfBronzeCards 
+ * 2) Имя лидера колоды 
+ * 3) Сила лидера колоды 
+ * II. Свойства: 
+ * 1) Количество карт в колоде (карта лидера не входит в количество карт других типов) 
+ * 2) Виртуальная свойство: цитата колоды
+ * III. Методы: 
+ * 1) Виртуальный метод ProbabilityToWinTheGame(), вычисляющий вероятность выигрыша партии случайным образом в процентах. Вероятность – вещественное число. 
+ * 2) Переопределенный метод ToString() для вывода информации о колоде, включая общее количество карт. 
+ * 
+ * Члены класса Nilfgaard:
+ * I. Поля: 
+ * 1) Сила способности колоды
+ * 2) Уловка (поле строкового типа)
+ * II. Виртуальное свойство: цитата фракции
+ * III. Методы:
+ * 1) Виртуальный метод ProbabilityToWinTheGame(), вычисляющий вероятность выигрыша партии случайным образом в процентах. Вероятность – вещественное число. 
+ * 2) Переопределенный метод ToString() для вывода информации о колоде, включая общее количество карт. 
+ * IV. Конструктор общего вида с семью параметрами
+ *
+ * Члены класса Skellige:
+ * I. Поля: 
+ * 1) Сила способности колоды
+ * 2) Количество медиков в колоде
+ * II. Виртуальное свойство: цитата фракции
+ * III. Методы:
+ * 1) Виртуальный метод ProbabilityToWinTheGame(), вычисляющий вероятность выигрыша партии случайным образом в процентах. Вероятность – вещественное число. 
+ * 2) Переопределенный метод ToString() для вывода информации о колоде, включая общее количество карт. 
+ * IV. Конструктор общего вида с семью параметрами
+ */
+using System;
+using Fractions;
+
+namespace Example_Task_01
+{
+    class Program
+    {
+        /// <summary>
+        /// Генератор псевдослучайных чисел
+        /// </summary>
+        static Random random = new Random();
+        /// <summary>
+        /// Метод для нахождения случайной буквы
+        /// </summary>
+        /// <param name="left">Левая граница диапазона</param>
+        /// <param name="right">Правая граница диапазона</param>
+        /// <returns></returns>
+        public static char GetRandomLetter(char left, char right)
+        {
+            int Letter = random.Next((int)left, (int)right + 1);
+            return (char)Letter;
+        }
+        /// <summary>
+        /// Метод для нахождения рандомного слова
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRandomSubterfuge()
+        {
+            string Subterfuge = string.Empty;
+            int length = random.Next(3, 11);
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 0) Subterfuge += GetRandomLetter('A', 'Z').ToString();
+                else Subterfuge += GetRandomLetter('a', 'z').ToString();
+            }
+            return Subterfuge;
+        }
+        static void Main(string[] args)
+        {
+            // NOTE: Запустите программу несколько раз. 
+            // Получив результаты, уберите из определения класса Skellige модификаторы virtual и вновь запустите приложение. Объясните и исправьте ошибки.
+            Deck deck = new Deck(5, 6, 14, "Brouver Hoog", 4);
+            Console.WriteLine($"Deck info:\n{deck}");
+            Console.WriteLine($"Probability to win the game: {deck.ProbabilityToWinTheGame():f2}%");
+            Console.WriteLine(deck.Quote);
+            deck = new Nilfgaard(4, 7, 16, "Jan Calveit", 5, 40, GetRandomSubterfuge());
+            Console.WriteLine($"\nDeck info:\n{deck}");
+            Console.WriteLine($"Probability to win the game on Nilfgaard: {deck.ProbabilityToWinTheGame():f2}%");
+            Console.WriteLine(deck.Quote);
+            deck = new Skellige(6, 6, 15, "Crach an Craite", 5, 12, 6);
+            Console.WriteLine($"\nDeck info:\n{deck}");
+            Console.WriteLine($"Probability to win the game on Skellige: {deck.ProbabilityToWinTheGame():f2}%");
+            Console.WriteLine(deck.Quote);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2module/Seminar07/Примеры решённых задач/Virtual methods/Fractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Fractions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fractions")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7d1182af-79d1-463c-969c-72069d118f45")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
