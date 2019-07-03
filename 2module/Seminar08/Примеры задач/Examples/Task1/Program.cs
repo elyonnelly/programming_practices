ea6575932e8b282534e9af37f662ac2ae304b248
Александр Мануйлов <aamanuylov@edu.hse.ru>
2019-07-03 13:22:14 +0300

    Сделаны две задачи для примеров

--- 2module/Seminar08/Примеры задач/Examples/Task1/Program.cs
+++ 2module/Seminar08/Примеры задач/Examples/Task1/Program.cs
@@ -10,6 +10,68 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Triangle[] arr = new Triangle[3];
+            arr[0] = new Triangle(0, 0, 1, 0, 0, 1);
+            arr[1] = new Triangle(-1, -1, 5, 5, 6, 0);
+            arr[2] = new Triangle(-1, 0, 0, 1, 1, 0);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public double Distance(Point point) // вычисляет расстояние между точками
+        {
+            return Math.Sqrt((this.X - point.X) * (this.X - point.X) + (this.Y - point.Y) * (this.Y - point.Y));
+        }
+
+        public override string ToString()
+        {
+            return $"(X = {X}, Y = {Y})";
+        }
+    }
+
+    class Triangle
+    {
+        public Triangle(double ax, double ay, double bx, double by, double cx, double cy) // Конструктор, осуществяющий
+        { // отношение композиции между классами Point и Triangle
+            A = new Point(ax, ay);
+            B = new Point(bx, by);
+            C = new Point(cx, cy);
+        }
+
+        public Point A { get; private set; } // Вершины треугольника
+        public Point B { get; private set; }
+        public Point C { get; private set; }
+
+        public double ABSide { get => A.Distance(B); } // Длины сторон треугольника
+        public double BCSide { get => B.Distance(C); }
+        public double CASide { get => C.Distance(A); }
+
+        public double Area() // Вычисление площади
+        {
+            double p = (ABSide + BCSide + CASide) / 2;
+            return Math.Sqrt(p * (p - ABSide) * (p - BCSide) * (p - CASide));
+        }
+
+        public override string ToString()
+        {
+            return $"Triangle:   Point A: {A.ToString()}, Point B: {B.ToString()}, Point B: {B.ToString()}, Area = {this.Area()}";
         }
     }
 }
---
+++ 2module/Seminar08/Примеры задач/Examples/Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            int occupantsNumber = rnd.Next(2, 10);
+            Occupant[] occupants = new Occupant[occupantsNumber];
+
+            for (int i = 0; i < occupantsNumber; i++)
+            {
+                occupants[i] = new Occupant(GenerateName(), rnd.Next(1, 100));
+            }
+
+            House house = new House(occupants);
+
+            Console.WriteLine(house);
+            Console.ReadKey();
+        }
+
+        static string GenerateName()
+        {
+            string res = String.Empty;
+            int length = rnd.Next(5, 15);
+            for (int i = 0; i < length; i++)
+            {
+                res += (char)rnd.Next('a', 'z');
+            }
+            return res.Substring(0, 1).ToUpper() + res.Substring(1);
+        }
+    }
+
+    class House
+    {
+        Occupant[] occupants;
+
+        public House(Occupant[] occupants)
+        {
+            this.occupants = occupants;
+        }
+
+        public override string ToString()
+        {
+            string res = "Occupants: \n";
+            for (int i = 0; i < occupants.Length; i++)
+            {
+                res += occupants[i].ToString() + "\n";
+            }
+            return res;
+        }
+    }
+
+    class Occupant
+    {
+        public Occupant(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
