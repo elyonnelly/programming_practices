443db5349809eb2f7085839c2ede4de872a9bad5
elyonnelly <alena@kotim.ru>
2019-07-03 16:00:00 +0300

    Добавлен пример на абстрактные классы и методы

---
+++ 2module/Seminar07/Примеры решённых задач/Seminar2_7_Example2/TransportLibrary/Ship.cs
@@ -0,0 +1,31 @@
+﻿namespace TransportLibrary
+{
+    /// <summary>
+    /// Класс корабля. Доставка груза по воде
+    /// </summary>
+    public class Ship : Transport
+    {
+        public Ship(int capacity) : base(capacity)
+        {
+
+        }
+
+        /// <summary>
+        /// Переопределенный метод доставки товаров по морю
+        /// </summary>
+        /// <param name="departure">Пункт отправки груза</param>
+        /// <param name="destination">Пункт назначения груза</param>
+        /// <param name="cargo">Груз</param>
+        /// <returns></returns>
+        public override string Deliver(City departure, City destination, Cargo cargo)
+        {
+            if (destination.IsThereAPort && destination.Loads.Count < destination.Capacity)
+            {
+                destination.Loads.Add(cargo);
+                return $"Сargo {cargo} successfully delivered from {departure.Name} to {departure.Name} by sea";
+            }
+
+            return $"Cargo {cargo} not delivered";
+        }
+    }
+}
