630ba2add11981be98a117b6c51dbed5239cfd17
Александр Мануйлов <aamanuylov@edu.hse.ru>
2019-07-04 17:44:54 +0300

    Добавлены примеры задач

--- 3module/Seminar01/Примеры задач/Examples/Task3/Properties/AssemblyInfo.cs
+++ 3module/Seminar01/Примеры задач/Examples/Task3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Task3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Task3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("50148542-d4b4-4ba4-969a-20c8e83b561a")]
+[assembly: Guid("bf89c850-87dc-45b5-a89b-03c4ae5ec1e4")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
