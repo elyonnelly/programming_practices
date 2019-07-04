5b2b180c28dd385acbce2e98ce2ab0b5c4ac3f52
Fomin Sergey <sdfomin@edu.hse.ru>
2019-07-04 18:04:56 +0300

    2_06 Примеры решённых задач

---
+++ 2module/Seminar06/Примеры решённых задач/ExampleTasks/HumanLibrary/Rand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanLibrary
+{
+    public static class Rand
+    {
+        public static Random rng = new Random();
+
+        public static int Next() => rng.Next();
+        public static int Next(int x) => rng.Next(x);
+        public static int Next(int left, int right) => rng.Next(left, right);
+        public static double NextDouble() => rng.NextDouble();
+        public static bool NextBool() => rng.Next(0, 2) == 1 ? true : false;
+    }
+
+}
---
+++ 2module/Seminar06/Примеры решённых задач/ExampleTasks/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Студент: Фомин Сергей
+ * Группа: БПИ182
+ * Дата: 04.07.2019
+ * Задача: показать работу адаптеров потоков
+ */
+
+using HumanLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        private const int NumberOfStudents = 20;
+
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("../../../students.txt", FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                for (int i = 0; i < NumberOfStudents; ++i)
+                {
+                    sw.WriteLine(Human.GetHuman());
+                }
+            }
+        }
+    }
+}
