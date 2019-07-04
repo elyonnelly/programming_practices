0df80018441d20ee39cd987b061e3eda0900f7b1
geremere <rad.imamow@yandex.ru>
2019-07-04 12:43:12 +0300

    CW6

---
+++ Самостоятельные работы/6 самостояльная работа/CW6Virtual/ClassLibrary/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClassLibrary
+{
+    public abstract class Cetartiodactyla
+    {
+      
+        public virtual string Voice()
+        {
+            return "Song";
+        }
+        bool IsTail { get; set; }
+
+        protected Cetartiodactyla(bool isTail, double size, ConsoleColor skinColor)
+        {
+            IsTail = isTail;
+            Size = size;
+            SkinColor = skinColor;
+        }
+
+        double Size { get; set; }
+        string Tail
+        {
+            get
+            {
+                if (IsTail)
+                    return "has tail";
+                else
+                    return "has not tail";
+            }
+        }
+        ConsoleColor SkinColor { get; }
+        public override string ToString() => $"Hoofed\nColor: {SkinColor}\t{Tail}\tSize: {Size:f3}";
+    }
+    public class Whale : Cetartiodactyla
+    {
+        public Whale(bool isTail, double size, ConsoleColor skinColor,string type) : base(isTail, size, skinColor)
+        {
+            Type = type;
+        }
+
+        string Type { get; }
+        public override string Voice()
+        {
+            Console.Beep(440, 300);
+            Console.Beep(415, 300);
+            Console.Beep(415, 300);
+            return $"Whale Song";
+        }
+        public override string ToString()
+        {
+            return $"Whale\tType: {Type}\n{base.ToString()}";
+        }
+    }
+    public class Hippopotamus : Cetartiodactyla
+    {
+        public Hippopotamus(bool isTail, double size, ConsoleColor skinColor,double speed) : base(isTail, size, skinColor)
+        {
+            Power = speed;
+        }
+
+        double Power { get; set; }
+        public override string Voice()
+        {
+            Console.Beep(247, 500);
+            Console.Beep(417, 500);
+            Console.Beep(417, 500);
+            Console.Beep(370, 500);
+            Console.Beep(417, 500);
+            return $" Hippopotamus Song";
+
+        }
+        public override string ToString()
+        {
+            return $"Dolphin\tSpeed: {Power:f3}\n{base.ToString()}";
+        }
+    }
+}
---
+++ Самостоятельные работы/6 самостояльная работа/CW6Virtual/Exmample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Exmample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cetartiodactyla whale = new Whale(true,123,(ConsoleColor)3,"Blue");
+            whale.Voice();
+            Console.WriteLine(whale);
+            Cetartiodactyla dolphin = new Hippopotamus(true, 123, (ConsoleColor)3, 123.3);
+            dolphin.Voice();
+            Console.WriteLine(dolphin);
+            Console.Read();
+        }
+    }
+}
