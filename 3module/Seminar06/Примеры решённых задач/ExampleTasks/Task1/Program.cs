d0e94f737920fb580c2b257b6a47b31418bf614e
Fomin Sergey <sdfomin@edu.hse.ru>
2019-07-04 16:38:51 +0300

    3_06 Часть примеров задач

---
+++ 3module/Seminar06/Примеры решённых задач/ExampleTasks/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Студент: Фомин Сергей
+ * Группа: БПИ182
+ * Дата: 04.07.2019
+ * Задача: показать работу потоков
+ * https://docs.microsoft.com/ru-ru/dotnet/api/system.threading.thread?view=netframework-4.8
+ */
+
+using System;
+using System.Threading;
+
+namespace ExampleTasks
+{
+    public class Program
+    {
+        public static void ThreadProc()
+        {
+            Console.WriteLine("ThreadProc: Я запустился!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: Делаю работу, {0}/10", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Main thread: Я запустился!");
+            Thread[] t = new Thread[4];
+            for (int i = 0; i < 4; ++i)
+                t[i] = new Thread(new ThreadStart(ThreadProc));
+            for (int i = 0; i < 4; ++i)
+                t[i].Start();
+
+            //Thread.Sleep(0);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Main thread: Делаю работу, {i}/4");
+                Thread.Sleep(0);
+            }
+
+            Console.WriteLine("Main thread: Вызываю Join() для ожидания завершения остальных потоков.");
+            for (int i = 0; i < 4; ++i)
+                t[i].Join();
+            Console.WriteLine("Main thread: Работа потоков завершена!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3module/Seminar06/Примеры решённых задач/ExampleTasks/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Студент: Фомин Сергей
+ * Группа: БПИ182
+ * Дата: 04.07.2019
+ * Задача: Пример передачи параметров без создания класса
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+
+    class Program
+    {
+        public static void PrintNumber(object obj)
+        {
+            // Если в кортежи тяжело, то можно создать класс/структуру ThreadArgs,
+            // передавать ThreadArgs и приводить obj к ThreadArgs
+            var (index, count) = (Tuple<int, int>)obj;
+
+            for (int i = 0; i < count; ++i)
+            {
+                Console.WriteLine($"PrintNumber[{index}]: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(PrintNumber);
+            Thread thread2 = new Thread(PrintNumber);
+
+            thread1.Start(new Tuple<int, int>(1, 100));
+            thread2.Start(new Tuple<int, int>(2, 100));
+            thread1.Join();
+            thread2.Join();
+            Console.ReadKey();
+        }
+    }
+}
