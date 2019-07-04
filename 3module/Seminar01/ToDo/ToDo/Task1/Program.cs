a96268a5b4ea4a1622fc8ea3cb81ed481d08fce7
geremere <rad.imamow@yandex.ru>
2019-07-04 17:48:16 +0300

    ToDo

---
+++ 3module/Seminar01/ToDo/ToDo/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        delegate double MyDelegate(double x);
+        static void Main(string[] args)
+        {
+            MyDelegate del1 = new MyDelegate(Pow);//ToDo реализовать с помощью анонимного метода
+            MyDelegate del2 = delegate (double x)
+            {
+                return Math.Sqrt(x);
+            };
+
+            Console.WriteLine($"{(del1 + del2)(double.Parse(Console.ReadLine())):f3}");//ToDo сделать так, чтобы выводился в модуль
+            Console.Read();
+        }
+        private static double Pow(double x) => x * x;
+
+    }
+
+}
