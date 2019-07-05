0d241d2061668ac87f68a9e24a76d2d1e06f1326
geremere <43412594+geremere@users.noreply.github.com>
2019-07-05 15:24:08 +0300

    Update Student.cs

--- 2module/Seminar03/Examples/Example/ClassLibrary/Student.cs
+++ 2module/Seminar03/Examples/Example/ClassLibrary/Student.cs
@@ -4,7 +4,7 @@ namespace ClassLibrary
 {
     public class Student
     {
-        public static int CountOfStudents;//количество студентов в вузе
+        public static int CountOfStudents{get;private set;}
         private int course;
         public int Course
         {
