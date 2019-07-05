09e0f084a094b7c03187d5dcee663c72ea89f773
Айнур Шарипов <aasharipov@edu.hse.ru>
2019-07-05 14:52:17 +0300

    Fixed

--- 2module/Seminar01/TODO задачи/Seminar_1_TODO/Task_1/Program.cs
+++ 2module/Seminar01/TODO задачи/Seminar_1_TODO/Task_1/Program.cs
@@ -29,12 +29,11 @@ static class MatrixOperations
             }
             return arr;
         }
-        else
-            return null;
+        return null;
     }
 
-    internal static int[,] Parser(string[] strs)   // internal можно, т.к. Program и 
-                                                   // находятся в одной сборке
+    internal static int[,] Parser(string[] strs)   // internal можно, т.к. Program и статический класс 
+                                                   // MatrixOperations находятся в одной сборке
     {
         // в Parser (методе парсере) мы возвращаем null, если из массива строк 
         // нельзя сформировать матрицу (значит, в других методах 
