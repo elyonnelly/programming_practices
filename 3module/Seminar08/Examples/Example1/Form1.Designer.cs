897a68c8a430e21d61f52b16e39ed58c60cba827
Александр Мануйлов <aamanuylov@edu.hse.ru>
2019-08-07 20:10:08 +0500

    Черновой вариант примера

---
+++ 3module/Seminar08/Examples/Example1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Example1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProcessing = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(69, 51);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(660, 28);
+            this.progressBar.TabIndex = 2;
+            // 
+            // lblProcessing
+            // 
+            this.lblProcessing.Location = new System.Drawing.Point(297, 15);
+            this.lblProcessing.Name = "lblProcessing";
+            this.lblProcessing.Size = new System.Drawing.Size(155, 33);
+            this.lblProcessing.TabIndex = 3;
+            this.lblProcessing.Text = "Processing ... 0%";
+            this.lblProcessing.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(29, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(130, 40);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 158);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblProcessing);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Monte Carlo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblProcessing;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ 3module/Seminar08/Examples/Example1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public double res = 0;
+        Func<double, double> func = (double x) => Math.Exp(-x * x); // Функция для поиска интеграла
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        { // метод Монте Карло
+            double x;
+            double y; // Случайная точка
+            double value; 
+            int square = 4; // Площадь на которой происходит интегрирование
+            int entries = 0; // Кол-во попаданий под график
+            int numberOfPoints = 5000; // Кол-во точек всего
+            int index = 1;
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                if (!backgroundWorker.CancellationPending)
+                {
+                    backgroundWorker.ReportProgress((int)(index++ * 100 / numberOfPoints));
+                    x = rnd.Next(-2, 2) + rnd.NextDouble();
+                    y = rnd.NextDouble();
+                    value = func(x);
+                    if (y < value) entries++;
+                }
+            }
+            res = entries / numberOfPoints * square;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        { // Отслеживаем изменение процесса
+            progressBar.Value = e.ProgressPercentage;
+            lblProcessing.Text = $"Processing ... {e.ProgressPercentage}%";
+            lblProcessing.Refresh();
+            progressBar.Update();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show($"Вычисление завершено \n Результат: {res}", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+    }
+}
