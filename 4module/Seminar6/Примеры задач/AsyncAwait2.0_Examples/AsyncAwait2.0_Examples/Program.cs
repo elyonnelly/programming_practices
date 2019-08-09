3dc6de0e1371c88be8af3f1663c7c1fc9ec06082
elyonnelly <alena@kotim.ru>
2019-08-09 17:44:48 +0500

    Добавлены примеры задач

---
+++ 4module/Seminar6/Примеры задач/AsyncAwait2.0_Examples/AsyncAwait2.0_Examples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Parallel LINQ -- это то, о чем вы мечтали.
+ */
+
+namespace AsyncAwait2._0_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
