ca8129d967b82a9f0143ed8d9eeba53a7a789a6d
elyonnelly <alena@kotim.ru>
2019-08-12 16:28:37 +0500

    Добавила материалы по структурам

---
+++ 3module/Seminar10/Примеры решённых задач/Interfaces_Example_Tasks/Struct_ClassLibrary/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Struct_Example_01
+{
+    public struct Triangle : IGeometry
+    {
+        private Point A, B, C;
+
+        /*
+         * Обратите внимание, что здесь происходит копирование значений! 
+         */
+        public Triangle(Point A, Point B, Point C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public double GetSquare() => Math.Abs((A.x - C.x) * (B.y - C.y) - (B.x - C.x) * (A.y - C.y)) * 0.5;
+
+        public double GetPerimeter() =>
+            new Vector(A, B).Length() + new Vector(B, C).Length() +
+            new Vector(A, C).Length();
+
+        public void DragOnVector(Vector v)
+        {
+            A += v;
+            B += v;
+            C += v;
+        }
+    }
+}
---
+++ 3module/Seminar10/Примеры решённых задач/Interfaces_Example_Tasks/Struct_ClassLibrary/Vector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Struct_Example_01
+{
+    public struct Vector
+    {
+        /// <summary>
+        /// Начало и конец вектора
+        /// </summary>
+        public Point A, B;
+
+        /*
+         * Обратите внимание, что здесь происходит копирование значений! 
+         */
+        public Vector(Point A, Point B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+
+        /// <summary>
+        /// Перегруженная операция сложения двух векторов
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public static Vector operator + (Vector v, Vector u)
+        {
+            return new Vector(v.A, v.B + u);
+        }
+
+        /// <summary>
+        /// Получить длину вектора
+        /// </summary>
+        /// <returns></returns>
+        public double Length() => Math.Sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+    }
+}
