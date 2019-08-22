afe92795e22c7e96bd4a4c6c8b3b31276cd80547
geremere <rad.imamow@yandex.ru>
2019-08-22 15:24:45 +0300

    mod4sem4 already

---
+++ 4module/Seminar4/HW/HW/HW/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HW
+{
+    public class Book
+    {
+        int id;
+        int Id
+        {
+            set
+            {
+                if (value <= 0) throw new ArgumentException("id целое положительное число");
+                id = value;
+            }
+        }
+        string name;
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (value.Length > 40) throw new ArgumentException("длина названия книги не должна превышать 40 символов");
+                name = value;
+            }
+        }
+        string author;
+
+        public Book(int id, string name, string author)
+        {
+            Id = id;
+            Name = name;
+            Author = author;
+        }
+
+        public string Author
+        {
+            get => author;
+            private set
+            {
+                if (value.Length > 25) throw new ArgumentException("длина имени автора не должна превышать 25 символов");
+                author = value;
+            }
+        }
+        public override string ToString() => $"{id}, {Name}, {Author}";
+
+
+    }
+}
---
+++ 4module/Seminar4/HW/HW/HW/Library.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HW
+{
+    public class Library
+    {
+        List<Book> books;
+
+        public Library(List<Book> books)
+        {
+            Books = books;
+        }
+
+        List<Book> Books
+        {
+            set
+            {
+                if (value == null) books = new List<Book>();
+                books = value;
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new LibraryEnumerator(this);
+        }
+        public class LibraryEnumerator : IEnumerator
+        {
+            List<Book> booksenum;
+
+            int position = -1;
+
+            public LibraryEnumerator(Library lib)
+            {
+                this.booksenum = lib.books;
+            }
+
+            public object Current => booksenum[position];
+
+            public bool MoveNext()
+            {
+                if (position < booksenum.Count - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+        public event EventHandler<NewBookEventArgs> NewBook;
+        public void Add(Book book)
+        {
+            books.Add(book);
+            NewBook(this, new NewBookEventArgs(book));
+        }
+    }
+
+}
